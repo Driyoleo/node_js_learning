b55bee92986cd01b68f6f1cc39d2319766bd4a33
driyo <debashisr433@gmail.com>
2024-05-10 00:04:07 +0530

    next.js with tailwind

---
+++ ex027_nextJs_tailwindCSS/l_tail/app/page.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <nav className=' bg-emerald-950 text-white p-10 w-screen fixed'>
+        <ul className=' flex justify-between items-center'>
+          <li className=' text-2xl'>BRITANIC</li>
+          <div className=' flex items-center gap-14'>
+            <li><a href="">Login</a></li>
+            <li><a href="">Subscribe</a></li>
+          </div>
+        </ul>
+      </nav>
+      <main className='pt-40 '>
+        <div className='side '>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reiciendis necessitatibus aspernatur voluptate. Nulla quis nobis a non facere, ipsa eveniet odio accusantium veniam sequi ex officiis sunt aspernatur beatae suscipit, unde excepturi? Numquam nobis eius consectetur voluptatem dolores autem magni, iste iusto porro quibusdam cupiditate, aut architecto laborum praesentium eos laboriosam, aperiam doloremque. Consequatur nihil nemo soluta reprehenderit ad dolores unde corrupti rem voluptate possimus provident, iusto ipsum modi iste eos ab facere repellat autem asperiores veniam esse optio molestias aspernatur neque! Quidem necessitatibus distinctio vero voluptatum, minus esse facere, magni mollitia nisi dignissimos maiores fugit nam impedit voluptas. Rerum mollitia ad reiciendis accusamus delectus aliquid fuga architecto magnam pariatur quas doloremque sint vel quos explicabo ullam laborum quo molestias molestiae harum sed, sequi repellat id aliquam consectetur! Cumque, numquam accusantium itaque inventore ea, odio voluptatum vero dolores in sint excepturi nam iusto velit. Et, hic, vitae ut asperiores suscipit praesentium ipsa consectetur, quidem eveniet quibusdam esse aliquid impedit necessitatibus. Quis, cupiditate enim. Saepe reiciendis quis excepturi, repellat impedit facilis vero eligendi deserunt tempora beatae libero aspernatur cumque dicta voluptatibus consequatur ut commodi deleniti aliquam reprehenderit? Ut iure repudiandae repellendus a vel nisi natus molestiae dolorum iusto? Ab, distinctio autem!</div>
+        <div className='main'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur saepe, deserunt a, est quos asperiores eaque repellendus fuga optio deleniti ducimus esse expedita beatae repellat cumque accusantium eligendi consectetur placeat cum! Mollitia adipisci architecto earum amet quo soluta ut optio voluptates numquam, error sequi dolorum debitis rem recusandae iusto quis corporis ipsam dicta autem, molestias laudantium voluptatem dignissimos saepe ex. Qui, saepe temporibus? Quidem reiciendis harum placeat laudantium, ipsa magni. Aperiam vero atque accusamus eaque ab eum et? Quos quas minima consequatur velit eaque debitis, veniam sequi, tempora voluptates vel, fuga aut quam animi fugit facilis obcaecati quo reprehenderit hic tempore. Quae animi iusto magni eius necessitatibus sapiente quibusdam quis totam ipsam. Fugit doloribus repudiandae neque consequatur saepe rerum optio aperiam nulla consectetur libero vero, suscipit quo animi maxime exercitationem aut quae obcaecati, eveniet sunt similique dolores sint dolore expedita quam. Odio laboriosam, vel esse nihil dolores maiores sequi enim quibusdam velit repudiandae, reiciendis fugiat unde iure eos perspiciatis sunt corporis voluptas fuga nobis consequatur laudantium. Facilis error illo adipisci? Facilis nam inventore rerum repellendus, ut fugit minima dignissimos corporis pariatur fuga? Voluptate, numquam ipsam nihil esse ab officia voluptas nam minima, ullam commodi debitis cum illo. Delectus, itaque nostrum?</div>
+      </main>
+    </div>
+  )
+}
+
+export default page
